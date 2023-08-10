bb5032e21b61ad8cc4e4a01b21edafa9e016bab9
julionagaita2 <111884078+julionagaita2@users.noreply.github.com>
2023-08-10 11:08:18 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -3,45 +3,58 @@ let arrayIDs = [
   document.querySelector('.atividades'),
   document.querySelector('div.divInput'),
   document.querySelector('#botaoAdicionar'),
-  
 ];
 
+let contador = parseInt(localStorage.getItem('contador')) || 0;
+let arrayIdAtividades = [];
 
-arrayIDs[0].addEventListener('click', corFundo);
 arrayIDs[0].addEventListener('click', salvarAtividade);
 arrayIDs[3].addEventListener('click', criarPrimeiraAtividade);
-let contador = 0;
+
 function criarPrimeiraAtividade() {
   contador++;
+  localStorage.setItem('contador', contador.toString());
+  criarAtividade(contador);
+}
+
+window.addEventListener('load', function () {
+  for (let i = 1; i <= contador; i++) {
+    const conteudo = localStorage.getItem('atividade' + i);
+    if (conteudo) {
+      criarAtividade(i, conteudo);
+    }
+  }
+});
+
+function criarAtividade(id, conteudo = '') {
   let novaDiv = document.createElement('div');
   let novotextArea = document.createElement('textarea');
-
+  
   let novoP = document.createElement('p');
   let novoInputSalvar = document.createElement('input');
-
+  
   novaDiv.classList.add('divInput');
   novotextArea.required = true;
   novotextArea.autocomplete = 'off';
-  novotextArea.id = 'atividade' + contador;
+  novotextArea.id = 'atividade' + id;
   novotextArea.classList.add('atividades');
+  novotextArea.value = conteudo;
+  
   novoInputSalvar.type = 'button';
   novoInputSalvar.value = 'Salvar';
   novoInputSalvar.classList.add('enviarAtividadeBotao');
-
   novaDiv.appendChild(novotextArea);
-  
   novoP.appendChild(novoInputSalvar);
   novaDiv.appendChild(novoP);
   
   const main = document.querySelector('.main');
   main.appendChild(novaDiv);
-
+  
   novotextArea.addEventListener('keyup', e => {
-    novotextArea.style.height = "auto";
+    novotextArea.style.height = 'auto';
     let scrollAltura = e.target.scrollHeight;
     novotextArea.style.height = `${scrollAltura}px`;
   });
-  
   
   novoInputSalvar.addEventListener('click', salvarAtividade);
 }
@@ -53,57 +66,14 @@ function salvarAtividade() {
   localStorage.setItem(id, conteudo);
 }
 
-window.addEventListener('load', function () {
-  for (let i = 0; i < localStorage.length; i++) {
-    const key = localStorage.key(i);
-    const conteudo = localStorage.getItem(key);
-    if (key.startsWith('atividade')) {
-      const textarea = document.getElementById(key);
-      if (textarea) {
-        textarea.value = conteudo;
-      } else {
-        criarNovaAtividade(key, conteudo);
-      }
-    }
-  }
-});
-function criarNovaAtividade(id, conteudo) {
-  let novaDiv = document.createElement('div');
-  let novotextArea = document.createElement('textarea');
-  
-  let novoP = document.createElement('p');
-  let novoInputSalvar = document.createElement('input');
-  novaDiv.classList.add('divInput');
-  novotextArea.required = true;
-  novotextArea.autocomplete = 'off';
-  novotextArea.id = id;
-  novotextArea.classList.add('atividades');
-  novotextArea.value = conteudo;
-
-  
-  novoInputSalvar.type = 'button';
-  novoInputSalvar.value = 'Salvar';
-  novoInputSalvar.classList.add('enviarAtividadeBotao');
-  novaDiv.appendChild(novotextArea);
-  novoP.appendChild(novoInputSalvar);
-  novaDiv.appendChild(novoP);
-  const main = document.querySelector('.main');
-  main.appendChild(novaDiv);
-  novotextArea.addEventListener('keyup', e => {
-    novotextArea.style.height = "auto";
-    let scrollAltura = e.target.scrollHeight;
-    novotextArea.style.height = `${scrollAltura}px`;
-  });
-  novoInputSalvar.addEventListener('click', salvarAtividade);
-}
 for (let i = 1; i <= contador; i++) {
   const textarea = document.getElementById('atividade' + i);
+
   if (textarea) {
     textarea.addEventListener('keyup', e => {
-      textarea.style.height = "auto";
+      textarea.style.height = 'auto';
       let scrollAltura = e.target.scrollHeight;
       textarea.style.height = `${scrollAltura}px`;
     });
   }
 }
-
